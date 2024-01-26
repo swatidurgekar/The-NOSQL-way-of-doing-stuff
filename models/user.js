130b1166af7c852e1a5c15f5be2b9d80709fbfea
swatidurgekar <swatidurgekar@gmail.com>
2024-01-26 22:52:35 +0530

    Cart System

--- models/user.js
+++ models/user.js
@@ -24,6 +24,54 @@ const userSchema = new Schema({
   },
 });
 
+userSchema.methods.addToCart = function (product) {
+  const cartProductIndex = this.cart.items.findIndex((cp) => {
+    return cp.productId.toString() === product._id.toString();
+  });
+  let newQuantity = 1;
+  const updatedCartItems = [...this.cart.items];
+
+  if (cartProductIndex >= 0) {
+    newQuantity = this.cart.items[cartProductIndex].quantity + 1;
+    updatedCartItems[cartProductIndex].quantity = newQuantity;
+  } else {
+    updatedCartItems.push({
+      productId: product._id,
+      quantity: newQuantity,
+    });
+  }
+  const updatedCart = {
+    items: updatedCartItems,
+  };
+  this.cart = updatedCart;
+  return this.save();
+};
+
+userSchema.methods.getCart = function () {
+  const productIds = this.cart.items.map((i) => {
+    return i.productId;
+  });
+  // return db
+  //   .collection("products")
+  return (
+    this.cart.items
+      // .find({ _id: { $in: productIds } })
+      .find()
+      .toArray()
+      .then((products) => {
+        console.log(products);
+        return products.map((p) => {
+          return {
+            ...p,
+            quantity: this.cart.items.find((i) => {
+              return i.productId.toString() === p._id.toString();
+            }).quantity,
+          };
+        });
+      })
+  );
+};
+
 module.exports = mongoose.model("User", userSchema);
 
 // const mongodb = require("mongodb");
